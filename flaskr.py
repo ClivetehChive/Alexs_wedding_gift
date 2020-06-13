e004ccc12a3b43b49281ca483ea6772528fdab27
ClivetehChive <alexvandenhof@yahoo.co.uk>
2020-06-13 17:04:51 +0100

    Added variable passing example

--- flaskr.py
+++ flaskr.py
@@ -7,7 +7,7 @@ app.register_blueprint(example_blueprint)
 
 @app.route('/')
 def home():
-    return redirect(url_for('example.test'))
+    return redirect(url_for('example.test', variable='This is the passed variable'))
 
 if __name__ == '__main__':
     app.run()
--- views/placeholder.py
+++ views/placeholder.py
@@ -2,6 +2,6 @@ from flask import Flask, Blueprint
 
 example_blueprint = Blueprint('example', __name__)
 
-@example_blueprint.route('/spotify')
-def test():
-    return "This is the blueprint"
+@example_blueprint.route('/<variable>', methods=['POST', 'GET'])
+def test(variable):
+    return variable
