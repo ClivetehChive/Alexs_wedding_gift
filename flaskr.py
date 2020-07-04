3fb814c57d38104fb0d48b0f49536db50852d333
Ubuntu <ubuntu@ip-172-31-25-143.us-east-2.compute.internal>
2020-07-04 19:52:23 +0000

    fixed merge

--- flaskr.py
+++ flaskr.py
@@ -1,14 +1,8 @@
 import numpy
-<<<<<<< HEAD
-from flask import Flask, redirect, url_for
-from .views.placeholder import example_blueprint
-from . import app
-=======
 from Alexs_wedding_gift import app
 from flask import Flask, redirect, url_for
 import views.SpControl as spControl
 from flask_bootstrap import Bootstrap
->>>>>>> 2327c9f7eda434e6a14a59357316f31f9ba264c4
 
 app.secret_key = "tempKey"
 app.register_blueprint(spControl.spControl)
